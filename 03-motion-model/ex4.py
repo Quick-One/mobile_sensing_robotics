ae1c4af648fd0d0b1884ae96c6b1ac2cf2262374
Quick-One <73107127+Quick-One@users.noreply.github.com>
2023-08-08 00:47:43 +0530

    Completed Motion model

--- 03-motion-model/ex4.py
+++ 03-motion-model/ex4.py
@@ -1,4 +1,5 @@
 from math import atan2, hypot, sqrt, pi
+import numpy as np
 
 def inverse_motion_model(X1, X2):
     x_, y_, theta_ = X1
@@ -36,4 +37,29 @@ def coord(x,y):
     a = (x-75) * 0.01 + 2
     b = (y-75) * 0.01 + 3
     return (a,b,0)
+
+def sample_motion_model_odometry(initial_pose, odometry, noise_alpha):
+    a1, a2, a3, a4 = noise_alpha
+    x0, x1 = odometry
+    x,y,theta = initial_pose
+
+    odelta_rot1, odelta_trans, odelta_rot2 = inverse_motion_model(x0, x1)
+    hdelta_rot1 = odelta_rot1 + sample_triangle(  a1* abs(odelta_rot1) + a2 * odelta_trans )
+    hdelta_trans = odelta_trans + sample_triangle(a3 * odelta_trans + a4 *(abs(odelta_rot1) + abs(odelta_rot2)))
+    hdelta_rot2 = odelta_rot2 + sample_triangle( a1 * abs(odelta_rot2) + a2 * odelta_trans)
+
+    x_ = x + np.cos(theta + hdelta_rot1) * hdelta_trans
+    y_  = y + np.sin(theta + hdelta_rot1) * hdelta_trans
+    theta_ = theta + hdelta_rot1 + hdelta_rot2
+    return (x_, y_, theta_)
+
+
+def sample_triangle(b):
+    return sqrt(6)/2 * (np.random.uniform(-b,b) + np.random.uniform(-b,b))
+
+
+def inver_coord(x,y):
+    a = (x-2) * 100 + 75
+    b = (y-3) * 100 + 75
+    return (a,b)
 
